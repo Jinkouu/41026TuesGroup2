45566c667c83085f3381d17d241aba29392e4368
Jiaxing13818671 <1430356245@qq.com>
2022-10-02 17:33:56 +1100

    update unit test

--- Tests/dailyTest.php
+++ Tests/dailyTest.php
@@ -7,7 +7,7 @@ class dailyTest extends TestCase
 {
 
     public function test01(): void
-    {//测试搜索上海
+    {//test search shanghai
         $apiData = file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=shanghai&appid=3794141fe0cac15a9225a73d70d21ce8");
         $weather_array =json_decode($apiData, true);
         $tempCelsius = $weather_array['main']['temp'] - 273;
@@ -15,12 +15,12 @@ class dailyTest extends TestCase
         $humidity=strval($weather_array['main']['humidity']);
         $dailyData = file_get_contents($GLOBALS['host']."daily.php?city=shanghai&submit=");
         $Feels = strval(intval($tempCelsius));
-        $this->assertStringContainsString($Feels."°C",$dailyData);//判断温度
-        $this->assertStringContainsString($wind."km/h",$dailyData);//判断风力
-        $this->assertStringContainsString($humidity."%",$dailyData);//humidity
+        $this->assertStringContainsString($Feels."°C",$dailyData);
+        $this->assertStringContainsString($wind."km/h",$dailyData);
+        $this->assertStringContainsString($humidity."%",$dailyData);
     }
     public function test02(): void
-    {//测试搜索空
+    {
         $apiData = file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=beijing&appid=3794141fe0cac15a9225a73d70d21ce8");
         $weather_array =json_decode($apiData, true);
         $tempCelsius = $weather_array['main']['temp'] - 273;
@@ -29,8 +29,8 @@ class dailyTest extends TestCase
         $dailyData = file_get_contents($GLOBALS['host']."/daily.php?city=&submit=");
         $Feels = strval(intval($tempCelsius));
         $this->assertStringContainsString($Feels."°C",$dailyData);
-        $this->assertStringContainsString($wind."km/h",$dailyData);//判断风力
-        $this->assertStringContainsString($humidity."%",$dailyData);//humidity
+        $this->assertStringContainsString($wind."km/h",$dailyData);
+        $this->assertStringContainsString($humidity."%",$dailyData);
     }
 
 }
--- Tests/fiveDaysTest.php
+++ Tests/fiveDaysTest.php
@@ -7,12 +7,12 @@ class fiveDaysTest extends TestCase
 {
 
     public function test01(): void
-    {//测试搜索上海
+    {//test search shanghai
         $apiData2 = file_get_contents("http://api.openweathermap.org/data/2.5/forecast?q=shanghai&appid=3794141fe0cac15a9225a73d70d21ce8");
         $seven = json_decode($apiData2, true);
         $fiveData = file_get_contents($GLOBALS['host']."/fiveDays.php?city=shanghai&submit=");
         $n=0;
-        foreach ($seven['list'] as $v) {//循环断言：判断fiveDays的天气
+        foreach ($seven['list'] as $v) {
             if (strpos($v['dt_txt'],"21:00:00")!==false) {
                 if($n!==0){
                     echo $v['dt_txt']."<br>";
@@ -21,10 +21,10 @@ class fiveDaysTest extends TestCase
                     $wind=strval($v['wind']['speed']);
                     $pressure=strval($v['main']['pressure']);
                     $humidity=strval($v['main']['humidity']);
-                    $this->assertStringContainsString($wind."m/s",$fiveData);//判断风力
+                    $this->assertStringContainsString($wind."m/s",$fiveData);
                     $this->assertStringContainsString($Feels."℃",$fiveData);
-                    $this->assertStringContainsString($humidity."%",$fiveData);//humidity
-                    $this->assertStringContainsString($pressure."hpa",$fiveData);//humidity
+                    $this->assertStringContainsString($humidity."%",$fiveData);
+                    $this->assertStringContainsString($pressure."hpa",$fiveData);
                 }
                 $n++;
             }

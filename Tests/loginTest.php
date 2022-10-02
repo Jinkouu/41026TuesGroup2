ff0c7d1169291e166526e1325509f4ae555f862d
Jinkouu <72260420+Jinkouu@users.noreply.github.com>
2022-10-02 23:59:05 +1100

    login test

--- Tests/loginTest.php
+++ Tests/loginTest.php
@@ -1,8 +1,25 @@
 <?php
 //jonathan
+use GuzzleHttp\Client;
 class loginTest extends \PhpUnit\Framework\TestCase{
-    //public function testLogin(){
-        
-    //}
+    public function testLogin(){
+        // create our http client (Guzzle)
+        $client = new Client([
+            // Base URI is used with relative requests
+            'base_uri' => 'http://localhost/41026TuesGroup2/login.php',
+            'timeout'  => 2.0,
+        ]);
+
+        $response = $client->request('POST', 'http://localhost/41026TuesGroup2/login.php/post', [
+            'form_params' => [
+                'uname' => 'wrong',
+                'password' => 'doesntExist',
+            ]
+        ]);
+
+        //checks post response
+        $code = $response->getStatusCode();
+        $this->assertEquals(200, $response->getStatusCode());
+    }
 }
 ?>
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit1ccad0e2a7786628f5eebc6f9f2a610f
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit1ccad0e2a7786628f5eebc6f9f2a610f', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit1ccad0e2a7786628f5eebc6f9f2a610f', 'loadClassLoader'));

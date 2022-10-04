908da4a0a4a50b91d2997b4ed55ddd60caa2423c
Jinkouu <72260420+Jinkouu@users.noreply.github.com>
2022-10-04 14:52:16 +1100

    more comments

--- Tests/weatherMapTest.php
+++ Tests/weatherMapTest.php
@@ -2,12 +2,15 @@
 //jonathan
 class weatherMapTest extends \PhpUnit\Framework\TestCase{
     public function testWeatherMap(){
+        //tests the api
         $apiData = file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=sydney&appid=3794141fe0cac15a9225a73d70d21ce8");
         $weather_array =json_decode($apiData, true);
         $tempCelsius = $weather_array['main']['temp'] - 273;
         $weatherMain = $weather_array['weather'][0]['main'];
         $Feels = strval(intval($tempCelsius));
         $dailyData = file_get_contents($GLOBALS['host']."daily.php?city=sydney&submit=");
+
+        //asserts the strings
         $this->assertStringContainsString($Feels."°C",$dailyData);
         $this->assertStringContainsString($weatherMain,$weatherMain);
     }

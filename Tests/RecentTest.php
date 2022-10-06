cc6cc3b936632eb173fd11af1408ca62e6f1210c
mirmidion43 <69776783+mirmidion43@users.noreply.github.com>
2022-10-07 10:58:10 +1100

    recentTest updates

--- Tests/RecentTest.php
+++ Tests/RecentTest.php
@@ -1,18 +1,33 @@
 <?php
-
+//Tim Fitzgerald
 namespace Tests;
 use PHPUnit\Framework\TestCase;
-
-class RecentTest extends TestCase
+use GuzzleHttp\Client;
+session_start();
+class recentTest extends TestCase
 {
-    public function testBasicTest(): void
-    {
+    public function testRecent(){
+        //establish client
+        $client = new Client([
+            'base_uri' => 'http://localhost/41026TuesGroup2/index.php',
+            'timeout'  => 2.0,
+        ]);
+        
+        //submit GET request
+        $response = $client->request('GET', 'http://localhost/41026TuesGroup2/index.php?city=Sydney&submit=');
+            
+        //checks GET response
+        $code = $response->getStatusCode();
+        $this->assertEquals(200, $response->getStatusCode());
 
+        //checks for the presence of the recent search
+        $body = $response->getBody();
+        $this->assertMatchesRegularExpression('/Re-search 1/',$body);
 
-        self::assertTrue()->assertStatus(200);
+        //clears the session so the test will not overload
+        session_unset();
     }
-
-
+    
 }
-
+?>
 

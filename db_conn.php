8fa49523b9aa8fad2f1e6fe0d459afabb7248fec
Jinkouu <jonathan0174@live.com>
2022-10-19 00:11:05 +1100

    database

--- db_conn.php
+++ db_conn.php
@@ -1,14 +1,14 @@
 <?php 
 
 $sname = "41026database.mysql.database.azure.com";
-$unmae = "jonathan";
+$unmae = "jonathan@41026database";
 $password = "password0.";
 
 $db_name = "test";
 
 $conn = mysqli_init();
 
-mysqli_real_connect($conn, $sname, $unmae, $password, $db_name, 3360);
+mysqli_real_connect($conn, $sname, $unmae, $password, $db_name, 3360, NULL, MYSQLI_CLIENT_SSL);
 
 if (mysqli_connect_errno()){
     echo "Connection failed!";
